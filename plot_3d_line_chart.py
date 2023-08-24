a4bba0c4022caa1b5cdc1d12980010ef5f01890f
rain.cchio <530007971@qq.com>
2023-08-24 16:06:41 +0800

    3d line chart

---
+++ plot_3d_line_chart.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from utils import get_results, get_parsed_dict, smooth_results
+import os
+
+domains = ['domain1', 'domain2','domain3','domain4',]
+algos = ['algo1', 'algo2', 'algo3', 'algo4']
+paths = ["task"]
+domain_algo_plot, algo_domain_path, retrived_algos = get_parsed_dict(paths)
+
+for id, algo in enumerate(retrived_algos):
+    if algo in algos:  # remove null results algo
+        retrived_algos.pop(id)
+algos.extend(retrived_algos)  # include more algo and keep the algo index not change
+
+# COLORS = ['#77AC30', '#A56DB0', "#F0C04A", '#DE6C3A', '#2988C7', '#0000FF']
+COLORS = ["#ccb974", '#8172b2', '#c44e52', '#55a868', '#4c72b0', '#0000FF']
+
+metrics = ["metric2"]
+figsize=()
+ax = plt.figure().add_subplot(projection='3d')
+
+max_len = 0
+max_y_value = 0
+for algo in algos:
+    pre_domain = None
+    color = COLORS[algos.index(algo)]
+    for idx, domain in enumerate(domains):
+        data_path = os.path.join(paths[0], algo, domain) # can  get multiple metrics result
+        res_dict = get_results(data_path, metrics=metrics)
+        single_metric_res = res_dict[metrics[0]]
+        results = smooth_results(single_metric_res)
+        mean = np.mean(results, axis=1)
+
+        x_vals = np.arange(len(mean))  # x axis item interval
+
+        if pre_domain:
+            for yidx in range(0, len(mean), 5):
+                ax.plot([pre_domain['x'], idx], [yidx,yidx], [pre_domain['z'][yidx],mean[yidx]], color=color, alpha=0.2)
+
+        pre_domain = {
+            'x': idx,
+            'z': mean
+        }
+
+        max_len = max(max_len, x_vals[-1])
+
+
+ax.set_ylabel('million steps')
+ax.set_xlabel('domain')
+ax.set_zlabel('average return')
+# ax.legend()
+
+# plt.tight_layout()
+ax.view_init(elev=20, azim=45, roll=0)
+timestr = time.strftime("%Y%m%d-%H%M%S")
+plt.savefig('./pdf/3dlinechart.pdf',  dpi=300)
--- plot_line_chart.py
+++ plot_line_chart.py
@@ -1,78 +1,8 @@
-import os
-import random
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+from utils import get_results, get_parsed_dict, smooth_results
 
-# Set Matplotlib font and style settings
-rc_fonts = {
-    'xtick.direction': 'in',
-    'ytick.direction': 'in',
-    'xtick.labelsize': 10,
-    'ytick.labelsize': 10,
-    'ytick.right': True,
-    'xtick.top': True,
-    "font.family": "times",
-    "font.size": 10,
-    'axes.titlesize': 10,
-    "legend.fontsize": 10,
-    # "axes.spines.right": False,
-    # "axes.spines.top": False,
-    # 'figure.figsize': (8, 3.5),
-}
-plt.rcParams.update(rc_fonts)
-plt.rc('axes', unicode_minus=False)
-
-def get_results(algo_domain_path, metrics):
-    res_dict = {}
-    seeds = os.listdir(algo_domain_path)
-    for seed in seeds:
-        csv_path = os.path.join(algo_domain_path, seed, 'progress.csv')
-        print('load file:', csv_path)
-        data = np.genfromtxt(csv_path, delimiter=',', names=True, dtype=float)
-        for metric in metrics:
-            if metric in res_dict.keys():
-                res_dict[metric].append(data[metric])
-            else:
-                res_dict[metric] = [data[metric]]
-    for metric in metrics:
-        min_row = min([len(col) for col in res_dict[metric]])
-        clip_res = [col[0:min_row] for col in res_dict[metric]]
-        res_dict[metric] = np.stack(clip_res, -1)
-    return res_dict
-
-def smooth_results(results, smoothing_window=100):
-    smoothed = np.zeros_like(results)
-    for idx in range(len(smoothed)):
-        if idx == 0:
-            smoothed[idx] = results[idx]
-            continue
-        start_idx = max(0, idx - smoothing_window)
-        smoothed[idx] = np.mean(results[start_idx:idx], axis=0)
-    return smoothed
-
-def get_parsed_dict(paths):
-    """
-    path format: .../task/algo/domain/seed/progress.csv
-    input: paths is a list of mlutiple task paths
-    """
-    domain_algo_plot = {}  # dict for plotting figure
-    algo_domain_path = {}  # dict for loading data
-    algos = []             # list for collecting all algo
-    for path in paths:
-        algos_ = os.listdir(path)
-        algos.extend(algos_)
-        for algo in algos_:
-            domains = os.listdir(os.path.join(path, algo))
-            for domain in domains:
-                if domain not in domain_algo_plot.keys():
-                    domain_algo_plot[domain] = [algo]
-                else:
-                    domain_algo_plot[domain].append(algo)
-                assert algo + '-' + domain not in algo_domain_path.keys()
-                algo_domain_path[algo + '-' + domain] = os.path.join(path, algo, domain)
-    print('retrieved algos', algos)
-    return domain_algo_plot, algo_domain_path, algos
 
 domain = 'domain1'
 algos = ['algo1', 'algo2', 'algo3', 'algo4']
