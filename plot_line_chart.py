61f708f88555068f088d1c55388ff08e30b1ad56
rain.cchio <530007971@qq.com>
2023-08-26 13:19:30 +0800

    Update plot_line_chart.py

--- plot_line_chart.py
+++ plot_line_chart.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 from utils import get_results, get_parsed_dict, smooth_results
 
 
-domain = 'domain1'
+domains = ['domain1']
 algos = ['algo1', 'algo2', 'algo3', 'algo4']
-paths = ["task"]
+paths = ["./task"]
+task = 'compare'
 domain_algo_plot, algo_domain_path, retrived_algos = get_parsed_dict(paths)
 
 for id, algo in enumerate(retrived_algos):
@@ -21,51 +21,49 @@ LINES = ['-', '--', ':']
 metrics = ["metric1"]
 
 # Plot line chart
-plt.figure(figsize=(4.8, 4.2)) # width and height
-# Plot lines
-max_len = 0
-max_y_value = 0
-for algo in domain_algo_plot[domain]:
-    data_path = algo_domain_path[algo + '-' + domain] # can  get multiple metrics result
-    res_dict = get_results(data_path, metrics=metrics)
-    single_metric_res = res_dict[metrics[0]]
-    results = smooth_results(single_metric_res)
-    mean = np.mean(results, axis=1)
-    std = np.std(results, axis=1)
+for domain in domains:
+    plt.clf()
+    plt.figure(figsize=(4.8, 4.2)) # width and height
+    max_x_len = 0
+    max_y_value = -1e6
+    min_y_value = 1e6
+    for algo in domain_algo_plot[domain]:
+        data_path = algo_domain_path[algo + '-' + domain] # can  get multiple metrics result
+        res_dict = get_results(data_path, metrics=metrics)
+        single_metric_res = res_dict[metrics[0]]
+        results = smooth_results(single_metric_res)
+        mean = np.mean(results, axis=1)
+        std = np.std(results, axis=1)
 
-    x_vals = np.arange(len(mean))  # x axis item interval
+        x_vals = np.arange(len(mean))  # x axis item interval
 
-    color = COLORS[algos.index(algo)]
-    marker = MARKERS[algos.index(algo) % 4 ]
-    line = LINES[algos.index(algo)//3]
-    marker_num = 8
-    makerevery = x_vals[-1] // marker_num
-    plt.plot(x_vals, mean, label=algo, marker=marker, markevery=makerevery, markerfacecolor='none', markersize=5.5, markeredgewidth=1.5, color=color, linestyle=line)
-    plt.fill_between(x_vals, mean - std, mean + std, color=color, alpha=0.3)
+        color = COLORS[algos.index(algo)]
+        marker = MARKERS[algos.index(algo) % 4 ]
+        line = LINES[algos.index(algo)//3]
+        marker_num = 8
+        makerevery = x_vals[-1] // marker_num
+        plt.plot(x_vals, mean, label=algo, marker=marker, markevery=makerevery, markerfacecolor='none', markersize=5.5, markeredgewidth=1.5, color=color, linestyle=line)
+        plt.fill_between(x_vals, mean - std, mean + std, color=color, alpha=0.3)
 
-    max_len = max(max_len, x_vals[-1])
-    max_y_value = max(max_y_value, np.max(mean+std))
+        max_x_len = max(max_x_len, x_vals[-1])
+        max_y_value = max(max_y_value, np.max(mean+std))
+        min_y_value = max(min_y_value, np.min(mean + std))
 
-# Plot misc
-plt.ylabel('reward')
-plt.xlabel('million steps')
+    # Plot misc
+    plt.ylabel('reward')
+    plt.xlabel('million steps')
 
-x_ticks_interval = 1000  # just want three ticks, let it be max_len//3
-x_tick_interval = 0.5
-x_max_value = 3.1
-x_ticks_values = np.arange(0, x_max_value, x_tick_interval)
-x_ticks = [x * x_ticks_interval for x in x_ticks_values]
-plt.xticks(x_ticks, x_ticks_values)
-plt.xlim(0, x_ticks[-1])
-plt.ylim(0, np.ceil(max_y_value))
-plt.ticklabel_format(style='sci', scilimits=(0, 0), axis='y')
+    x_tick_interval = max_x_len//3  # just want five ticks, let it be max_len//5
+    x_ticks = np.arange(0, max_x_len+1, x_tick_interval)
+    plt.xticks(x_ticks, np.around(x_ticks/x_tick_interval, 1))
+    plt.xlim(0, x_ticks[-1]+1)
+    plt.ylim(np.ceil(min_y_value), np.ceil(max_y_value))
+    plt.ticklabel_format(style='sci', scilimits=(0, 0), axis='y')
 
-plt.grid(True, linestyle='-', alpha=0.5)
-lgd = plt.legend(loc='lower right', bbox_to_anchor=(1, 0), ncol=2, fancybox=False, framealpha=1, edgecolor='black', prop={'size': 8})
-lgd.get_frame().set_alpha(None)
-lgd.get_frame().set_facecolor((0, 0, 0, 0))
-plt.tight_layout()
+    plt.grid(True, linestyle='-', alpha=0.5)
+    lgd = plt.legend(loc='lower right', bbox_to_anchor=(1, 0), ncol=2, fancybox=False, framealpha=1, edgecolor='black', prop={'size': 8})
+    lgd.get_frame().set_alpha(None)
+    lgd.get_frame().set_facecolor((0, 0, 0, 0))
+    plt.tight_layout()
 
-timestr = time.strftime("%Y%m%d-%H%M%S")
-plt.savefig('./png/linechart.png', bbox_inches='tight',  dpi=300)
-# print('./plotting/pdf/'+task+'.pdf ','plot finished!')
+    plt.savefig('./png/'+task+'_'+domain+'.png', bbox_inches='tight',  dpi=300)
